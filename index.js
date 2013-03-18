0f5bd6f7a71e1120632456c1cc636e30c993b724
Raynos <raynos2@gmail.com>
2013-03-18 00:33:05 -0700

    fix typo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var server = require("tryme/server")
 var path = require("path")
 
 server({
-    root: [path.join(__dirname, "node_modules", "graphics", "examples")]
+    root: path.join(__dirname, "node_modules", "graphics", "examples")
 })
